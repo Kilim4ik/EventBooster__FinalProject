3b69be1c617a93292983a56c125a42b0a91a792f
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2024-12-24 19:01:02 +0200

    add fetch events with key

---
+++ src/js/fetch-events-data-with-key.js
@@ -0,0 +1,12 @@
+const URL =
+  'https://proxy.cors.sh/https://app.ticketmaster.com/discovery/v2/events.json?apikey=QcV4xVHAyk410RrmWruoaQyfKz244DeP';
+export const getEventsByName = (page, str) => {
+  return fetch(`${URL}&keyword=${str}&page=${page}`)
+    .then(res => res.json())
+    .then(data => data['_embedded'].events);
+};
+const showData = async () => {
+  const res = await getEventsByName(1, 'king');
+  console.log(res);
+};
+showData();
