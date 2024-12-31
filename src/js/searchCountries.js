d2ece21b1a30eb3b0ef4f693c484125676f6dff2
Sofia <putlyuksophia@gmail.com>
2024-12-31 12:53:30 +0200

    fix/searchCountries

--- src/js/searchCountries.js
+++ src/js/searchCountries.js
@@ -1,9 +1,15 @@
 import { getEventsByName } from "./fetch-events-data-with-key";
+import { renderEvents } from "./renderEvents";
 const searchCountriesButton = document.getElementById("buttonCountries")
 const searchInput = document.getElementById("searchInputCountries");
 searchCountriesButton.addEventListener('click',function(){
     const searchValue = searchInput.value.trim().toLowerCase();
+    let currentPage = 1;
     if(searchValue){
-        getEventsByName(1, searchText);
+        getEventsByName(currentPage, searchValue)
+        .then(events => {
+            renderEvents(events); 
+          })
+          .catch(error => console.error('Error loading events:', error));
     }
 })
